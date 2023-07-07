e9d58c0a952c4e9dadb7488df4b229c0b7503cb4
AlexeyKorotkov <alexlera@060511@gmail.com>
2023-07-07 15:45:52 +0300

    hw1 part2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import Character from "./Components/Character/Character";
+// id,name,status,species,gender,image
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Character id={1} name={'Rick Sanchez'} status={'Alive'} species={'Human'} gender={'Male'}
+                     image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
+          <Character id={2} name={'Morty Smith'} status={'Alive'} species={'Human'} gender={'Male'}
+                     image={"https://rickandmortyapi.com/api/character/avatar/2.jpeg"}/>
+          <Character id={3} name={'Summer Smith'} status={'Alive'} species={'Human'} gender={'Female'}
+                     image={"https://rickandmortyapi.com/api/character/avatar/3.jpeg"}/>
+          <Character id={4} name={'Beth Smith'} status={'Alive'} species={'Human'} gender={'Female'}
+                     image={"https://rickandmortyapi.com/api/character/avatar/4.jpeg"}/>
+          <Character id={5} name={'Jerry Smith'} status={'Alive'} species={'Human'} gender={'Male'}
+                     image={"https://rickandmortyapi.com/api/character/avatar/5.jpeg"}/>
+          <Character id={6} name={'Abadango Cluster Princess'} status={'Alive'} species={'Alien'} gender={'Female'}
+                     image={"https://rickandmortyapi.com/api/character/avatar/6.jpeg"}/>
+
+      </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Character/Character.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Character.css'
+
+const Character = (props) => {
+    let {id,name,status,species,gender,image}=props;
+    return (
+        <div className={'cartoon'}>
+            <h2>{id} {name} </h2>
+            <p>{status} {species} {gender}</p>
+            <img src={image} alt={name}/>
+
+        </div>
+    );
+};
+
+export default Character;
